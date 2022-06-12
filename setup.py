d6ef8a2efaab45e435212a536cfb6bd0553c12a7
Yaroslav Petrov <yaroslav.v.petrov@yandex.ru>
2022-06-12 09:27:25 +0000

    Transform repo into a library

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='aapm',
+    packages=find_packages(include='src'),
+    version='0.1.0',
+    description='My first Python library',
+    author='Me',
+    license='MIT',
+)
